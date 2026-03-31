388c71732ed7a03473ce6764bbe33a7d975c62c4
ejraman <ejraman@hotmail.com>
2026-03-31 21:34:57 +0800

    Update storage.py

--- storage.py
+++ storage.py
@@ -1,31 +1,23 @@
+import json
+import streamlit as st
 import gspread
 from google.oauth2.service_account import Credentials
-import streamlit as st
 
 SCOPES = [
     "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive",
 ]
 
 def get_sheets_client():
-    creds_info = {
-        "type": st.secrets["google_drive"]["type"],
-        "project_id": st.secrets["google_drive"]["project_id"],
-        "private_key_id": st.secrets["google_drive"]["private_key_id"],
-        "private_key": st.secrets["google_drive"]["private_key"],
-        "client_email": st.secrets["google_drive"]["client_email"],
-        "client_id": st.secrets["google_drive"]["client_id"],
-        "token_uri": "https://oauth2.googleapis.com/token",
-    }
+    creds_info = json.loads(st.secrets["google_service_account_json"])
     creds = Credentials.from_service_account_info(creds_info, scopes=SCOPES)
     return gspread.authorize(creds)
 
 def log_to_sheets(record: dict):
-    """Append one receipt record as a row in Google Sheet."""
     client = get_sheets_client()
     spreadsheet_id = st.secrets["google_sheets"]["spreadsheet_id"]
     sheet = client.open_by_key(spreadsheet_id).sheet1
 
-    # Write headers if sheet is empty
     existing = sheet.get_all_values()
     if not existing:
         sheet.append_row(list(record.keys()))
