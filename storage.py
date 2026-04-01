8f9f46998706241dcf2b2864119c66453e001d66
ejraman <ejraman@hotmail.com>
2026-04-01 12:19:40 +0800

    Update storage.py

--- storage.py
+++ storage.py
@@ -1,31 +1,24 @@
 import gspread
 from google.oauth2.service_account import Credentials
 import streamlit as st
+import json
 
 SCOPES = ["https://www.googleapis.com/auth/spreadsheets"]
 
 def get_sheets_client():
-    creds_info = {
-        "type": st.secrets["google_drive"]["type"],
-        "project_id": st.secrets["google_drive"]["project_id"],
-        "private_key_id": st.secrets["google_drive"]["private_key_id"],
-        "private_key": st.secrets["google_drive"]["private_key"],
-        "client_email": st.secrets["google_drive"]["client_email"],
-        "client_id": st.secrets["google_drive"]["client_id"],
-        "token_uri": "https://oauth2.googleapis.com/token",
-    }
+    creds_info = json.loads(st.secrets["google_service_account_json"])
     creds = Credentials.from_service_account_info(creds_info, scopes=SCOPES)
     return gspread.authorize(creds)
 
 def load_donors_from_sheets():
     client = get_sheets_client()
-    sheet = client.open_by_key(st.secrets["google_drive"]["spreadsheet_id"]).worksheet("Donors")
+    sheet = client.open_by_key(st.secrets["google_sheets"]["spreadsheet_id"]).worksheet("Donors")
     rows = sheet.get_all_records()
     return rows
 
 def log_to_sheets(record: dict):
     client = get_sheets_client()
-    sheet = client.open_by_key(st.secrets["google_drive"]["spreadsheet_id"]).worksheet("Receipts")
+    sheet = client.open_by_key(st.secrets["google_sheets"]["spreadsheet_id"]).worksheet("Receipts")
     existing = sheet.get_all_values()
     if not existing:
         sheet.append_row(list(record.keys()))
